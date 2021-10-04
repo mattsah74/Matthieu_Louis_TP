20a92b048278274c9e1db6ba186e7b7029eb204e
matth <matth@PC-de-Matthieu>
2021-10-04 17:08:02 +0200

    TP1 exo 1

---
+++ TP1/manipNombresInt_Sahinidis_Marquet/src/manipnombresint_sahinidis_marquet/ManipNombresInt_Sahinidis_Marquet.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package manipnombresint_sahinidis_marquet;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author matth
+ */
+public class ManipNombresInt_Sahinidis_Marquet {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nb1, nb2;
+        System.out.println("Rentrez le premier nombre: ");
+        Scanner sc;
+        sc = new Scanner(System.in);
+        nb1 = sc.nextInt();
+        System.out.println("Rentrez le deuxieme nombre: ");
+        nb2 = sc.nextInt();
+        System.out.println("Vos nombres sont le: "+nb1+" et le  "+nb2);
+        System.out.println("La somme des deux est: "+(nb1+nb2));
+        System.out.println("La difference des deux est: "+(nb1-nb2));
+        System.out.println("La produit des deux est: "+(nb1*nb2));
+        
+        System.out.println("La somme des deux est: "+(nb1+nb2));
+    }
+    
+}
+
+ 
+
+
