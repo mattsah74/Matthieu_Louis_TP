a8aef5b2a16bd77686e663ccd8761d6fe9d31174
matth <matth@PC-de-Matthieu>
2021-10-04 14:38:52 +0200

    TP0 test

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,32 @@
+/*
+ * TP0 Excercice 1 
+ * Matthieu Sahinidis   
+ * Septembre 2021
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author matth
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        int var,var1;
+        var1=15;
+        var=var1+5;
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom=sc.nextLine();
+        System.out.println(prenom);
+    }
+}
